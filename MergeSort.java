2543d5afb915f219c1f3b83ee7256aa8f8e2f5ec
Yoan Schnee <yschnee@yahoo.com>
2018-06-05 17:47:53 +0200

    Update MergeSort.java

--- MergeSort.java
+++ MergeSort.java
@@ -5,17 +5,18 @@ public class MergeSort {
 	static int[] arr = {1, 6, 2, 7, 3, 4, 10, 9, 8};
 	static int[] aux = new int[arr.length];
 	
+	// main
 	public static void main(String[] args) {
 		sort(arr, aux, 0, arr.length-1);
 		for (int k=0; k<arr.length; k++) {
 			System.out.println(arr[k]);
 		};
 	}
-
+	
+	// merge the two sorted parts a[lo:mid] and a[mid+1:hi] of a in growing order
 	public static void merge(int[] a, int[] aux, int lo, int mid, int hi) {
 		for (int l=lo; l<=hi; l++) {
 			aux[l] = a[l];
-			System.out.println("l=" + l + " aux[l]:" + aux[l]);
 		}
 
 		int i=lo;
@@ -26,14 +27,12 @@ public class MergeSort {
 			else if (i > mid) a[k]=aux[j++];
 			else if (aux[i]<aux[j]) a[k]=aux[i++];
 			else a[k]=aux[j++];
-			System.out.println("k=" + k + " a[k]:" + a[k]);
 		}
 	}
-
+	// divide and conquer recursion to sort elements of array a
 	public static void sort(int[] a, int[] aux, int lo, int hi) {
 		if (hi<=lo) return;
 		int mid = (lo + (hi-lo)/2);
-		System.out.println("lo:" + lo + "  hi:" + hi + "  mid:" + mid);
 		sort(a, aux, lo, mid);
 		sort(a, aux, mid+1, hi);
 		merge(a, aux, lo, mid, hi);
